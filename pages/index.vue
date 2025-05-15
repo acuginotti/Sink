ed312ae0609910bd6ca1d6d791459843be5278a3
acuginotti <acuginotti@gmail.com>
2025-05-15 11:39:01 +0100

    first page forward

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  window.location.href = 'https://augustocuginotti.com'
+})
+</script>
+
 <template>
   <main class="flex flex-col justify-center">
     <HomeTwitter />
